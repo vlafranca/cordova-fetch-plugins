ae27497fd790a9a574751697edd7b340cba614b3
root <root@vps86543.ovh.net>
2016-10-11 13:23:43 +0200

    Repo init with first release

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cordova/Ionic plugin downloader utility
+
+This small command will help you save time when setting up an existing project on a new environment (CI for example), by automatically download all needed cordova plugins registered in the file `plugins/fetch.json`. Normally each installed plugin is registered into this file.
+
+# Install
+
+`npm install -g cordova-fetch-plugins`
+
+# Usage
+
+Execute this command at the root of your project :
+`fetch-plugins`
---
+++ bin/fetch-plugins.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+var fs = require("fs");
+var shell = require("shelljs");
+
+try {
+	var fetch = fs.readFileSync('plugins/fetch.json');
+
+	var plugins = JSON.parse(fetch);
+
+	for (var plugin in plugins) {
+	    var cordovaCmd = "cordova plugin add "+plugin;
+	    var variables = plugins[plugin].variables;
+	    if(Object.keys(variables).length > 0){
+	        for (var v in variables){
+	            cordovaCmd += " --variable "+v+"=\""+variables[v]+"\"";
+	        }
+	    }
+	    console.log("Executing command : "+cordovaCmd);
+	    shell.exec(cordovaCmd);
+	}
+}catch(err){
+	if (err.code === 'ENOENT') {
+	  console.log('Fetch.json file not found. Be sure to be at the root of your cordova project and plugins/fetch.json exist.');
+	} else {
+	  throw err;
+	}
+}
